37af2a19ce800a289a60776080f1cfa18b50eb07
blackout314 <blackout_lair@yahoo.com>
2015-05-13 14:23:27 +0200

    gruntfilepackage.json

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	build_dir: 'dist',
+	compile_dir: 'bin',
+
+	app_files: {
+		js: [ 'src/**/*.js' ],
+		views: [ 'src/**/*.html', 'src/**/*.png' ],
+		css: [ 'src/**/*.scss' ]
+	},
+
+	vendor_files: {
+		js: [
+		],
+		css: [
+		],
+		assets: [
+		]
+	},
+}
